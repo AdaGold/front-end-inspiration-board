e620f0bfaa7858e7103895c085a6f20b7e2c43cb
chan <cboswell1024@gmail.com>
2021-06-29 14:40:13 -0400

    cardList  component skeleton

---
+++ src/components/cardList.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types'
+import Card from './components/Card'
+
+
+const cardList = (props) => {
+    const cardComponents = props.Card.map((Card, index) => {
+        return (
+            <li key={index}>
+                <Card
+                    id='string'>
+                </Card>
+            </li>
+        );
+
+    });
+
+    return (
+        <section>
+            <h3>Card List</h3>
+            <ul>
+                {cardComponents}
+            </ul>
+        </section>
+    );
+};
+
+cardList.propTypes = {
+    cards: PropTypes.arrayOf(PropTypes.shape({
+        //whatever goes in here
+    })),
+};
+
+
+
+
+export default cardList;
