0f31e1753d007fc6d407a76c81d14e25702a2874
Raquel Mena <raquelmena03@gmail.com>
2021-06-28 22:30:44 -0500

    implemented NewBoardForm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import NewBoardForm from './components/NewBoardForm';
+const axios = require('axios');
+
+
 
 function App() {
+
+  // add new board, API call
+  const addBoard = (title, owner) => {
+    console.log("i'm in API")
+
+    axios.post('/user', {
+      title: title,
+      owner: owner
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Create New Board</h1>
+      <NewBoardForm addBoardCallback={addBoard}> </NewBoardForm>
+
+
+
+
+
+
+    </main>
   );
 }
 
---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const NewBoardForm = (props) => {
+    const [formFields, setFormFields] = useState({
+        title: "", owner: ""
+    });
+
+
+    const onTitleChange = (event) => {
+        setFormFields({
+            ...formFields,
+            title: event.target.value
+        })
+    }
+
+    const onOwnerChange = (event) => {
+        setFormFields({
+            ...formFields,
+            owner: event.target.value
+        })
+    }
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        props.addBoardCallback({
+            title: formFields.title,
+            owner: formFields.owner
+        });
+
+        setFormFields({
+            name: '',
+            email: '',
+        });
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div>
+                <label htmlFor="Title">Title</label>
+                <input
+                    name="Title"
+                    value={formFields.title}
+                    onChange={onTitleChange} />
+            </div>
+            <div>
+                <label htmlFor="owner">Owner</label>
+                <input name="owner"
+                    value={formFields.owner}
+                    onChange={onOwnerChange} />
+            </div>
+            <input
+                type="submit"
+                value="Add Board" />
+        </form>
+    );
+};
+
+export default NewBoardForm;
