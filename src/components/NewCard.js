4b2591f649f79c9dd8250441bfd9536441ccb3a4
chan <cboswell1024@gmail.com>
2021-06-29 15:36:49 -0400

    new message input

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -3,12 +3,23 @@ import propTypes from 'prop-types';
 import axios from 'axios';
 
 const NewCard = () =>{
+    const [cardMessage, setCardMessage] = useState('whatcha thinkin bout?');
+
+    const newCardMessage = (changeEvent) => {
+        console.log('new message')
+        console.log('message value:', changeEvent.target.value);
+        newCardMessage(changeEvent.target.value);
+    }
+    
     // no props passed in
     // state: holds information prior to submit
     // form handling: sends state to API
 
     return (
-        <h2>Placeholder New Card</h2>
+        <section>
+            <h2>{cardMessage}</h2>
+            <input type='text' value={cardMessage} onChange={newCardMessage} />
+        </section>
     );
 
 };
