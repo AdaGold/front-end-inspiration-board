8ce7440f21bef41857e5fbf107c2c4cd8b7abc4f
Raquel Mena <raquelmena03@gmail.com>
2021-06-29 17:31:22 -0500

    board titles display and get method implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import NewBoardForm from './components/NewBoardForm';
 import NewCardForm from './components/NewCardForm';
-import { useState } from 'react';
+import Board from './components/Board';
+import { useState, useEffect } from 'react';
 
 const axios = require('axios');
 
@@ -10,13 +11,13 @@ const axios = require('axios');
 function App() {
   //-----------------board states---------------------
   const [boardErrorMessage, setBoardErrorMessage] = useState("")
-  const [selectedBoard, setSelectedBoard] = useState({title: '', owner: '', board_id: null})
+  const [selectedBoard, setSelectedBoard] = useState({ title: '', owner: '', board_id: null })
   const [boardsData, setboardsData] = useState([])
 
   //-------------------card states----------------------
   const [cardErrorMessage, setCardErrorMessage] = useState("")
   const [cardsByBoardId, setCardsbyBoardId] = useState([])
-  
+
 
   // add new board, API call
   const addBoard = (boardData) => {
@@ -53,11 +54,27 @@ function App() {
   };
 
   //Get request that gets all boards and loops through them to display by title then saves as vriable to display in board display div. ea div will have a click function that vcalls get cards by board id. boards will be a list of objects
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
+    }).then((response) => {
+      console.log(response.data)
+      setboardsData(response.data);
+    })
+  }, [])
+
+  const boardList = boardsData.map((board) => {
+    return (<li>
+      <Board board={board}> </Board>
+    </li>)
+  });
+
+
 
   return (
     <main>
       <h1>INSPO BOARD</h1>
       <div>THIS WILL DISPLAY ALL boards</div>
+      <div>{boardList}</div>
       <NewBoardForm addBoardCallback={addBoard}> </NewBoardForm>
       <div>{boardErrorMessage}</div>
       <NewCardForm addCardCallback={addCard}></NewCardForm>
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,1 +1,12 @@
 //board will display name & owner of board... wondering if this is necessary or if card display could handle?
+
+
+const Board = (props) => {
+    return <div> {props.board.title} </div>
+
+
+
+
+}
+
+export default Board;
