914fb0f90d0350f12a7118175bc69c76a9ed3442
terahratheheart <terahbruce@gmail.com>
2021-07-01 13:55:47 -0700

    trying to get synced

--- src/App.js
+++ src/App.js
@@ -70,26 +70,6 @@ function App() {
     </li>)
   });
 
-  const cardLike = (cardId) => {
-    axios.put(`${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}/like`,)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error)
-      });
-  };
-
-  const cardDelete = (cardId) => {
-    axios.delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}`,)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error)
-      });
-  };
-
   return (
     <main className="main">
       <h1>INSPO BOARD</h1>
@@ -105,7 +85,7 @@ function App() {
       </section>
       <div className="cardError">{cardErrorMessage}</div>
       <section className="cardDisplay">
-        {selectedBoard.id ? <CardDisplay board={selectedBoard} likeCallBack={cardLike} deleteCallBack={cardDelete} addCardCallback={addCard}></CardDisplay> : ''}
+        {selectedBoard.id ? <CardDisplay board={selectedBoard} addCardCallback={addCard}></CardDisplay> : ''}
       </section>
     </main>
   );
