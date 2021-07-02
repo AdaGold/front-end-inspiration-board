237044533d7e8a7d072f5f2fe15aae37aadc350c
Drew Taylor <drewlynntaylor@gmail.com>
2021-07-01 17:55:35 -0700

    maybe a fix

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,7 @@
 // export default Board;
 
 const Board = (props) => {
-    return (<div onClick={() => props.onBoardSelect(props.board)}>{props.board.title}</div>);
+    return (<div onClick={() => props.onBoardSelect(props.board)}>{props.title}</div>);
 };
 
 export default Board;
