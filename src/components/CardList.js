ba83bb7a7da39e7c4b15c3baa6665fd004c182bc
RETURNcaden <cadence1547@gmail.com>
2021-07-13 12:18:16 -0700

    add the function for likes count

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Card from './Card'
 
-const CardList = ({ cards, deleteCard }) => {
+const CardList = ({ cards, deleteCard,likeCard }) => {
     return (
         <div>
             <h1>List of Card </h1>
             {cards && cards.length > 0 &&
                 cards.map(card => {
                     return (
-                        <Card key={card.card_id} card={card} deleteCard={deleteCard} />
+                        <Card key={card.card_id} card={card} deleteCard={deleteCard} likeCard={likeCard} />
                     )
                 })
             }
