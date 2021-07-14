d0336ae74ebb63c21eb63ac1d14d683f9d1abf4e
RETURNcaden <cadence1547@gmail.com>
2021-07-14 14:34:13 -0700

    fix the panda to center and fix the size of the new board form

--- src/components/NewBoard.js
+++ src/components/NewBoard.js
@@ -25,21 +25,24 @@ const NewBoard = ({ createBoard }) => {
     return (
         <div>
             <h1>Create A New Board</h1>
-            <div className='board-form'>
-                <form onSubmit={onFormSubmited}>
-                    <label className="label">
-                        Title:
-                        <input type='text' id='title' value={formData.title} onChange={textInput} />
-                    </label><br/>
-                    <label >
-                        Owner:
-                        <input type='text' id='owner' value={formData.owner} onChange={textInput} />
-                    </label>
-                    <p className='preview'>Preview:<br/>{formData.title} - {formData.owner}</p>
-                    <input className='submit-btn' type='submit'/>
+            {/* <div> */}
+                <form onSubmit={onFormSubmited} className='board'>
+                    <div style={{padding:'2em'}}>
+                        <label className="label">
+                            Title:
+                            <input type='text' id='title' value={formData.title} onChange={textInput} />
+                        </label><br/>
+                        <label >
+                            Owner:
+                            <input type='text' id='owner' value={formData.owner} onChange={textInput} />
+                        </label>
+                        <p className='preview'>Preview:<br/>{formData.title} - {formData.owner}</p>
+                        <input className='submit-btn' type='submit'/>
+                    </div>
                 </form>
+
                 {/* <span className='heart'>♡</span> */}
-            </div>
+            {/* </div> */}
 
         </div>
     )
