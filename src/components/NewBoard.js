d6556078b804877e8216a5c543ee5866f74842de
RETURNcaden <cadence1547@gmail.com>
2021-07-15 13:38:08 -0700

    fix the card and board scrolling

--- src/components/NewBoard.js
+++ src/components/NewBoard.js
@@ -26,30 +26,28 @@ const NewBoard = ({ createBoard }) => {
         <div>
             <h1>Create A New Board</h1>
          
-                <form onSubmit={onFormSubmited} className='board'>
-                    <div className="hearts">
-                        <div style={{margin:'2em auto', padding:'2em'}}>
-                            <label className="label">
-                                Title:
-                                <input type='text' id='title' value={formData.title} onChange={textInput} />
-                            </label><br/>
-                            <label >
-                                Owner:
-                                <input type='text' id='owner' value={formData.owner} onChange={textInput} />
-                            </label>
-                            <p className='preview'>Preview:<br/>{formData.title} - {formData.owner}</p>
-                            <div className="card-script">
-                                 <button className="submit-btn" type='submit'>Submit</button>
-                            </div>
-                        
-                            </div>
-                                        
-                            <div className="heart">♡</div>
-                            <div className="heart">♡</div>
-                            <div className="heart">♡</div>
-                            <div className="heart">♡</div>
-                    </div>
-                </form>      
+            <form onSubmit={onFormSubmited} className='board'>
+                <div className="hearts">
+                    <div style={{margin:'2em auto', padding:'2em'}}>
+                        <label className="label">
+                            Title:
+                            <input type='text' id='title' value={formData.title} onChange={textInput} />
+                        </label><br/>
+                        <label >
+                            Owner:
+                            <input type='text' id='owner' value={formData.owner} onChange={textInput} />
+                        </label>
+                        <p className='preview'>Preview:<br/>{formData.title} - {formData.owner}</p>
+                        <div className="card-script">
+                                <button className="submit-btn" type='submit'>Submit</button>
+                        </div>
+                    </div>  
+                    <div className="heart">♡</div>
+                    <div className="heart">♡</div>
+                    <div className="heart">♡</div>
+                    <div className="heart">♡</div>
+                </div>
+            </form>      
         </div>
     )
 }
