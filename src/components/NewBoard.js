83f43839fa4f11bfb87fe8fad9fc47c877159c97
Dena Burd <denaburd@gmail.com>
2021-07-14 20:09:02 -0700

    Updates toggleShowForm to clear the message in state and changes the - emoji to a X. Updates style for the newBoard submit button

--- src/components/NewBoard.js
+++ src/components/NewBoard.js
@@ -26,22 +26,24 @@ const NewBoard = ({ createBoard }) => {
         <div>
             <h1>Create A New Board</h1>
             {/* <div> */}
-                <form onSubmit={onFormSubmited} className='board'>
-                    <div style={{padding:'2em'}}>
-                        <label className="label">
-                            Title:
-                            <input type='text' id='title' value={formData.title} onChange={textInput} />
-                        </label><br/>
-                        <label >
-                            Owner:
-                            <input type='text' id='owner' value={formData.owner} onChange={textInput} />
-                        </label>
-                        <p className='preview'>Preview:<br/>{formData.title} - {formData.owner}</p>
-                        <input className='submit-btn' type='submit'/>
+            <form onSubmit={onFormSubmited} className='board'>
+                <div style={{ padding: '2em' }}>
+                    <label className="label">
+                        Title:
+                        <input type='text' id='title' value={formData.title} onChange={textInput} />
+                    </label><br />
+                    <label >
+                        Owner:
+                        <input type='text' id='owner' value={formData.owner} onChange={textInput} />
+                    </label>
+                    <p className='preview'>Preview:<br />{formData.title} - {formData.owner}</p>
+                    <div className="card-script">
+                        <button className="submit-btn" type='submit'>Submit</button>
                     </div>
-                </form>
+                </div>
+            </form>
 
-                {/* <span className='heart'>♡</span> */}
+            {/* <span className='heart'>♡</span> */}
             {/* </div> */}
 
         </div>
