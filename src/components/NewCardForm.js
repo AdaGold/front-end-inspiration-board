71aa9ece383557f7121d6ea60e7f3a4fc4ce3b58
Andrea Valliere <amvalliere@gmail.com>
2021-07-15 22:19:57 -0700

    adds new card form file

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,49 +1,28 @@
 import { useState } from 'react';
 import InspirationApi from '../api/inspirationApi';
 
-const NewBoardForm = ({setSelectedBoard}) => {
-  const defaultFormState = { title: '', owner: '' };
-  const [boardFormFields, setBoardFormFields] = useState(defaultFormState);
+const NewCardForm = ({setSelectedBoard}) => {
+  const [cardMessage, setCardMessage] = useState('');
 
-  const resetForm = () => {
-    setBoardFormFields(defaultFormState)
-  }
-
-  const onNewBoardSubmit = async (e) => {
+  const onNewCardSubmit = async (e) => {
     e.preventDefault();
-    const newBoard = {...boardFormFields}
-    const newCreatedBoard = await new InspirationApi().addBoard(newBoard);
-    setSelectedBoard(newCreatedBoard);
-    resetForm();
+    const newCreatedCard = await new InspirationApi().addCard(message);
+    setCardMessage('');
   };
 
-  const onTitleChange = (event) => {
-    setBoardFormFields({
-      ...boardFormFields,
-      title: event.target.value
-    })
-  };
-
-  const onOwnerChange = (event) => {
-    setBoardFormFields({
-      ...boardFormFields,
-      owner: event.target.value
-    })
+  const onMessageChange = (event) => {
+    setCardMessage(event.target.value)
   };
 
   return (
-    <form onSubmit={(e) => onNewBoardSubmit(e)}>
+    <form onSubmit={(e) => onNewCardSubmit(e)}>
       <label>
-        New Board Title:
-        <input type="text" value={boardFormFields.title} onChange={onTitleChange} />
-      </label>
-      <label>
-        Board Owner:
-        <input type="text" value={boardFormFields.owner} onChange={onOwnerChange} />
+        New Card:
+        <textarea maxLength="40" type="text" value={boardFormFields.title} onChange={onTitleChange} />
       </label>
       <input type="submit" value="Submit" />
     </form> 
   )
 }
 
-export default NewBoardForm;
+export default NewCardForm;
