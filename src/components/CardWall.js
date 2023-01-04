63bc850904097216ba7a0598d0831c12801029b2
Maxwell <maxpasqualis@gmail.com>
2023-01-04 12:39:11 -0800

    render all cards from board passed in through App

---
+++ src/components/CardWall.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Card from "./Card";
+
+const CardWall = (props) => {
+  console.log(props.cards);
+  const allCards = props.cards.map((card) => {
+    return (
+      <Card messageData={card.message} likesCountData={card.likes_count} />
+    );
+  });
+  return <div>{allCards}</div>;
+};
+
+CardWall.propTypes = {
+  cards: PropTypes.arrayOf(
+    PropTypes.shape({
+      message: PropTypes.string.isRequired,
+      likes_count: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default CardWall;
