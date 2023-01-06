c1cf63f9455c84123668ac216256c874ce33fed3
Maxwell <maxpasqualis@gmail.com>
2023-01-06 10:31:25 -0800

    initial commit

---
+++ src/components/CardForm.js
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+const CardForm = (props) => {};
+
+CardForm.propTypes = {
+  createCard: PropTypes.func.isRequired,
+};
+
+export default CardForm;
