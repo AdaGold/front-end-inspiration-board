a66343e60b592218a09959dc9f4fdd14701c6ed2
reneesu99 <reneesu22@gmail.com>
2023-01-17 14:23:19 -0800

    added new board form component and added it to app.js

--- src/Components/NewBoardForm.js
+++ src/Components/NewBoardForm.js
@@ -1,21 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "./NewBoardForm.css"
 
-const NewBoardForm = (props) => {
-    
-  const local = () =>{
+import { useState } from 'react';
 
-  }
-  return (
-    <div>
-      <h2></h2>
-      <section>
-        <p>hi</p>
-      </section>
-    </div>
-  );
+const NewBoardForm = () => {
+
+    const [formFields, setFormFields] = useState({
+        title: '',
+        owner: '',
+    });
+
+  
+    const handleChange = (event) => {
+      setFormFields({
+        ...formFields,
+        [event.target.id]: event.target.value,
+      });
+    };
+
+
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      setFormFields({
+        title:'',
+        owner: ''
+      })
+    }
+
+    return (
+      <div id="newBoardForm">
+        <h1> Add Board </h1>
+        <form onSubmit={onFormSubmit}>
+            <div>
+                <label htmlFor="title">Title:</label>
+                <input
+                    name="title"
+                    id="title"
+                    value={formFields.title}
+                    onChange={handleChange} />
+            </div>
+            <div>
+                <label htmlFor="owner">Owner:</label>
+                <input
+                    name="owner"
+                    id="owner"
+                    value={formFields.owner}
+                    onChange={handleChange} />
+            </div>
+            <input
+                type="submit"
+                value="Add Board" />
+        </form>
+      </div>
+    );
 };
-
 
 
 export default NewBoardForm;
