6d1f5deded2e91543b9c8df0d3c2effb92c44f0e
Tia Willis <tiawillis1@icloud.com>
2023-01-18 19:32:19 -0700

    handled merge confilt all code saved from all parties.

--- src/Components/BoardList.js
+++ src/Components/BoardList.js
@@ -9,7 +9,6 @@ const BoardList = (props) => {
   const [selectBoardTitle, setSelectedBoardTitle] = useState("Select a Board!");
   const [selectBoardOwner, setSelectedBoardOwner] = useState("");
   const boardComponents = props.boards.map((board) => {
-<<<<<<< HEAD
     return (
       <li
         onClick={() => {
@@ -19,6 +18,7 @@ const BoardList = (props) => {
         }}
       >
         {board.title}
+        <button onClick={() => props.deleteBoard(board)}>x</button>
       </li>
       // <Board
       //   key={board.id}
@@ -29,12 +29,6 @@ const BoardList = (props) => {
       //   selectBoard={props.selectBoard}
       // />
     );
-=======
-    return <li onClick={() => props.selectBoard(board)}>
-      {board.title} 
-      <button onClick={()=>props.deleteBoard(board)}>x</button>
-      </li>;
->>>>>>> a39880fd492b85414a421142969d26cbe26c9182
   });
 
   return (
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,34 +3,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Card = (props) => {
-<<<<<<< HEAD
   return (
-    <div className="cards">
-      <li>
-        <div>{props.message}</div>
-        <div> 💕{props.likes}</div>
-        <button>+1</button>
-        <button className="tasks__item__remove button">Delete</button>
-      </li>
-    </div>
+    <li>
+      <div>{props.message}</div>
+      <div> 💕{props.likes}</div>
+      <button onClick={() => props.updateLikes(props)}>+1</button>
+      <button
+        className="tasks__item__remove button"
+        onClick={() => props.deleteCard(props)}
+      >
+        Delete
+      </button>
+    </li>
   );
-=======
-    return (
-        <li>
-            <div>
-                {props.message}
-            </ div>
-            <button onClick={() => props.updateLikes(props)}>
-                {props.likes}
-            </button>
-            <button
-            className="tasks__item__remove button" 
-            onClick={() => props.deleteCard(props)}
-            >x
-            </button>
-        </li>
-    );
->>>>>>> a39880fd492b85414a421142969d26cbe26c9182
 };
 
 // Card.propTypes = {
